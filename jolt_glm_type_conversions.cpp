13e1ea1a6dbdcd811d3488efb7982765ef41fadc
cuppajoeman <ccn@cuppajoeman.com>
2025-02-03 02:36:53 -0500

    feat: correct include and pass by reference

--- jolt_glm_type_conversions.cpp
+++ jolt_glm_type_conversions.cpp
@@ -1,10 +1,10 @@
 #include "jolt_glm_type_conversions.hpp"
 
-JPH::Vec3 g2j(glm::vec3 input_vector) { return {input_vector.x, input_vector.y, input_vector.z}; }
+JPH::Vec3 g2j(const glm::vec3 &input_vector) { return {input_vector.x, input_vector.y, input_vector.z}; }
 
-glm::vec3 j2g(JPH::Vec3 input_vector) { return {input_vector.GetX(), input_vector.GetY(), input_vector.GetZ()}; }
+glm::vec3 j2g(const JPH::Vec3 &input_vector) { return {input_vector.GetX(), input_vector.GetY(), input_vector.GetZ()}; }
 
-glm::mat4 j2g(JPH::Mat44 input_matrix) {
+glm::mat4 j2g(const JPH::Mat44 &input_matrix) {
     glm::mat4 glm_mat;
     for (int col = 0; col < 4; col++) {
         for (int row = 0; row < 4; row++) {
--- jolt_glm_type_conversions.hpp
+++ jolt_glm_type_conversions.hpp
@@ -2,10 +2,10 @@
 #define JOLT_GLM_TYPE_CONVERSIONS_HPP
 
 #include <Jolt/Jolt.h>
-#include <glm/fwd.hpp>
+#include <glm/glm.hpp>
 
-JPH::Vec3 g2j(glm::vec3 input_vector);
-glm::vec3 j2g(JPH::Vec3 input_vector);
-glm::mat4 j2g(JPH::Mat44 input_matrix);
+JPH::Vec3 g2j(const glm::vec3 &input_vector);
+glm::vec3 j2g(const JPH::Vec3 &input_vector);
+glm::mat4 j2g(const JPH::Mat44 &input_matrix);
 
 #endif // JOLT_GLM_TYPE_CONVERSIONS_HPP
